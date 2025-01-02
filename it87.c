434df582d436f4262f877b3da013e32b0f1f9e0d
Flat <flat@esoteric.moe>
2025-01-02 16:56:34 -0500

    feat: add DMI Match for B650 AORUS PRO AX

--- it87.c
+++ it87.c
@@ -4554,6 +4554,9 @@ static const struct dmi_system_id it87_dmi_table[] __initconst = {
 		/* IT8689E */
 	IT87_DMI_MATCH_GBT("B650M GAMING X AX", it87_dmi_cb,
 			   &it87_acpi_ignore),
+		/* IT8689E*/
+	IT87_DMI_MATCH_GBT("B650 AORUS PRO AX", it87_dmi_cb,
+				&it87_acpi_ignore),
 		/* IT8689E */
 	IT87_DMI_MATCH_GBT("X670 AORUS ELITE AX", it87_dmi_cb,
 			   &it87_acpi_ignore),
